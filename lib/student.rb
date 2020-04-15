9fdc4488e7f2d33283fc4179b418dbb0c2401f94
Paul von Hagen <pvonhagen26@gmail.com>
2020-04-15 01:43:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -95,5 +95,14 @@ class Student
     end
   end
 
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      select * from students
+      where grade = ?
+      SQL
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
+  end
 
 end
